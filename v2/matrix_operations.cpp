0adaa847d6302b2a2c2c0d0602da116a4c7267d4
untrivial <axc@wharton.upenn.edu>
2024-08-14 21:50:00 -0400

    switch to vectors

--- v2/matrix_operations.cpp
+++ v2/matrix_operations.cpp
@@ -1,26 +1,33 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <iostream>
+#include <vector>
+#include <cmath>
 
 using namespace std;
 
 // Matrix Addition
 // Not CUDA
-void addMatrices(float *a, float *b, float *c, int m, int n) {
+void addMatrices(const vector<vector<float>>& a, const vector<vector<float>>& b, vector<vector<float>>& c) {
+    int m = a.size();
+    int n = a[0].size();
+    c.resize(m, vector<float>(n));
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
-            c[i * n + j] = a[i * n + j] + b[i * n + j];
+            c[i][j] = a[i][j] + b[i][j];
         }
     }
 }
 
 // Matrix Multiplication
 // Not CUDA
-void multiplyMatrices(float *a, float *b, float *c, int m, int n, int p) {
+void multiplyMatrices(const vector<vector<float>>& a, const vector<vector<float>>& b, vector<vector<float>>& c) {
+    int m = a.size();
+    int n = a[0].size();
+    int p = b[0].size();
+    c.resize(m, vector<float>(p, 0.0f));
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < p; j++) {
             for (int k = 0; k < n; k++) {
-                c[i * p + j] += a[i * n + k] * b[k * p + j];
+                c[i][j] += a[i][k] * b[k][j];
             }
         }
     }

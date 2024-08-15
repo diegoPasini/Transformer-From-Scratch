8b7d88fb5c3957dcf3c8b4a908020f4f546d06cd
diegoPasini <dpasini@wharton.upenn.edu>
2024-08-15 11:04:42 -0400

    Forward Pass

---
+++ v2/convolution.cpp
@@ -0,0 +1,111 @@
+#include <vector>
+#include <iostream>
+#include <cmath>
+#include <random>
+
+using namespace std;
+
+class Convolution {
+private:
+    int input_channels;
+    int output_channels;
+    int kernel_size;
+    int stride;
+    int padding;
+    vector<vector<vector<vector<float>>>> kernels; // [output_channels][input_channels][kernel_size][kernel_size]
+    vector<float> biases;
+
+    void initialize_kernels() {
+        random_device rd;
+        mt19937 gen(rd());
+        uniform_real_distribution<float> distr(-1.0, 1.0);
+
+        kernels.resize(output_channels, vector<vector<vector<float>>>(input_channels, vector<vector<float>>(kernel_size, vector<float>(kernel_size))));
+        for (int oc = 0; oc < output_channels; ++oc) {
+            for (int ic = 0; ic < input_channels; ++ic) {
+                for (int i = 0; i < kernel_size; ++i) {
+                    for (int j = 0; j < kernel_size; ++j) {
+                        kernels[oc][ic][i][j] = distr(gen);
+                    }
+                }
+            }
+        }
+
+        biases.resize(output_channels);
+        for (int oc = 0; oc < output_channels; ++oc) {
+            biases[oc] = distr(gen);
+        }
+    }
+
+    vector<vector<float>> pad_input(const vector<vector<float>>& input) {
+        int padded_size = input.size() + 2 * padding;
+        vector<vector<float>> padded_input(padded_size, vector<float>(padded_size, 0.0f));
+
+        for (int i = 0; i < input.size(); ++i) {
+            for (int j = 0; j < input[0].size(); ++j) {
+                padded_input[i + padding][j + padding] = input[i][j];
+            }
+        }
+
+        return padded_input;
+    }
+
+public:
+    Convolution(int in_channels, int out_channels, int k_size, int strd, int pad)
+        : input_channels(in_channels), output_channels(out_channels), kernel_size(k_size), stride(strd), padding(pad) {
+        initialize_kernels();
+    }
+
+    vector<vector<vector<float>>> forward(const vector<vector<vector<float>>>& input) {
+        int input_height = input[0].size();
+        int input_width = input[0][0].size();
+        int output_height = (input_height - kernel_size + 2 * padding) / stride + 1;
+        int output_width = (input_width - kernel_size + 2 * padding) / stride + 1;
+
+        vector<vector<vector<float>>> output(output_channels, vector<vector<float>>(output_height, vector<float>(output_width, 0.0f)));
+
+        for (int oc = 0; oc < output_channels; ++oc) {
+            for (int oh = 0; oh < output_height; ++oh) {
+                for (int ow = 0; ow < output_width; ++ow) {
+                    float sum = biases[oc];
+                    for (int ic = 0; ic < input_channels; ++ic) {
+                        for (int kh = 0; kh < kernel_size; ++kh) {
+                            for (int kw = 0; kw < kernel_size; ++kw) {
+                                int ih = oh * stride + kh - padding;
+                                int iw = ow * stride + kw - padding;
+                                if (ih >= 0 && ih < input_height && iw >= 0 && iw < input_width) {
+                                    sum += input[ic][ih][iw] * kernels[oc][ic][kh][kw];
+                                }
+                            }
+                        }
+                    }
+                    output[oc][oh][ow] = sum;
+                }
+            }
+        }
+
+        return output;
+    }
+
+    void print_kernels() const {
+        for (const auto& kernel : kernels) {
+            for (const auto& channel : kernel) {
+                for (const auto& row : channel) {
+                    for (float val : row) {
+                        cout << val << " ";
+                    }
+                    cout << endl;
+                }
+                cout << endl;
+            }
+            cout << endl;
+        }
+    }
+
+    void print_biases() const {
+        for (float val : biases) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+};
---
+++ v2/main.cpp
@@ -0,0 +1,60 @@
+#include "mnist_reader.h"
+#include "convolution.cpp"
+#include "functions.cpp"
+#include "linear_layer.cpp"
+#include "matrix_operations.cpp"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Load MNIST dataset
+    string train_images_path = "../MNIST Dataset/t10k-images-idx3-ubyte/t10k-images-idx3-ubyte";
+    string train_labels_path = "../MNIST Dataset/train-labels-idx1-ubyte/train-labels-idx1-ubyte";
+    
+    vector<vector<float>> train_images = read_mnist_images(train_images_path);
+    vector<uint8_t> train_labels = read_mnist_labels(train_labels_path);
+
+    cout << "Number of training images: " << train_images.size() << endl;
+    cout << "Number of training labels: " << train_labels.size() << endl;
+
+    Convolution conv1(1, 8, 3, 1, 1); // 1 input channel, 8 output channels, 3x3 kernel, stride 1, padding 1
+    Convolution conv2(8, 16, 3, 1, 1); // 8 input channels, 16 output channels, 3x3 kernel, stride 1, padding 1
+    LinearLayer fc1(16 * 28 * 28, 128); // Fully connected layer, input size 16*28*28, output size 128
+    LinearLayer fc2(128, 10); // Fully connected layer, input size 128, output size 10
+
+    vector<vector<vector<float>>> input_image(1, vector<vector<float>>(28, vector<float>(28)));
+    for (int i = 0; i < 28; ++i) {
+        for (int j = 0; j < 28; ++j) {
+            input_image[0][i][j] = train_images[0][i * 28 + j];
+        }
+    }
+
+    auto conv1_output = conv1.forward(input_image);
+    relu(conv1_output);
+    auto conv2_output = conv2.forward(conv1_output);
+    relu(conv2_output);
+
+    vector<float> flattened_output;
+    for (const auto& channel : conv2_output) {
+        for (const auto& row : channel) {
+            for (float val : row) {
+                flattened_output.push_back(val);
+            }
+        }
+    }
+
+    auto fc1_output = fc1.forward(flattened_output);
+    relu(fc1_output);
+    auto fc2_output = fc2.forward(fc1_output);
+    softmax(fc2_output);
+
+
+    cout << "Output of the network: ";
+    for (float val : fc2_output) {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
